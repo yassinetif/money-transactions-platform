803231383487b48d9502b37884ec1a0c47ae6d99
Williams de SOUZA <williamsko89@gmail.com>
2020-05-15 15:58:26 +0200

    feature : create notification model to store all sent sms, emails and their status

---
+++ apps/shared/models/notification.py
@@ -0,0 +1,23 @@
+from django.db import models
+from enum import Enum
+
+
+class NotificationType(Enum):
+    SMS = 'SMS'
+    EMAIL = 'EMAIL'
+
+
+class Notification(models.Model):
+    notification_type = models.CharField(max_length=30, choices=[
+        (tag.value, tag.value) for tag in NotificationType], default=NotificationType.SMS.value)
+    content = models.TextField(max_length=500)
+    content_receiver = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.notification_type)
+
+    class Meta:
+        verbose_name = "Notifications Log"
