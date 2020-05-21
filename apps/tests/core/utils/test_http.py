921534c16269a7ac3bf0fd9ac0be416d3205cf04
Williams de SOUZA <williamsko89@gmail.com>
2020-05-21 13:50:30 +0200

    whole refactor : rename tests folder

--- apps/tests/core/utils/test_http.py
+++
@@ -1,5 +0,0 @@
-# from apps.core.utils.http import post_json_request
-# def test_post_json_request(rf):
-#     request = rf.get('/customer/details')
-#     response = my_view(request)
-#     assert response.status_code == 200
