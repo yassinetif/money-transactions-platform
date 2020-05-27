699b9e501d121e98af16c2e4b9a2c188860128cf
Williams de SOUZA <williamsko89@gmail.com>
2020-05-27 18:26:42 +0200

    resolving issue identification_document_delivery_date synatax

--- apps/kyc/admin.py
+++ apps/kyc/admin.py
@@ -4,8 +4,7 @@ from apps.kyc.models import Customer, Cartera
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('informations', 'country', 'phone_number', 'identification_type',
-                    'identification_number', 'issuer_country', 'identification_document_delivery_date',
-                    'identification_document_expiry_date', 'address')
+                    'identification_number', 'issuer_country', 'address')
 
 
 admin.site.register(Customer, CustomerAdmin)
