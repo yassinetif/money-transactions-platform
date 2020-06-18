d9cec8121753b2453021b7a08d8786f78643bb38
Williams de SOUZA <williamsko89@gmail.com>
2020-06-18 11:56:03 +0200

    debug : add permission to accont admin

--- apps/shared/admin.py
+++ apps/shared/admin.py
@@ -21,10 +21,10 @@ class AccountAdmin(admin.ModelAdmin):
     account_object_currency.short_description = 'Devise'
 
     def has_add_permission(self, request):
-        return False
+        return True
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        return True
 
 
 admin.site.register(Account, AccountAdmin)
