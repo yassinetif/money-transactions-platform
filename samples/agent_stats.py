91dcc23ff3e3988d70d1d331be4cb2cec53825f7
Williams de SOUZA <williamsko89@gmail.com>
2020-09-22 16:55:56 +0200

    docs : api sample agent balance and stats

---
+++ samples/agent_stats.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+# Agent Stat
+token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiY29kZSI6Ijk3NDYxOCIsImV4cCI6MTYwMjMyMDI3M30.DCFVZU_mO0N_7dinu6a-2ivyOP9R9f4Wr2vWMxoqXWk'
+headers = {'Content-Type': 'application/json',
+           'Authorization': 'token {}'.format(token)}
+
+url = 'http://217.69.6.52/api/v1/transaction/agent/stats/'
+response = requests.post(url, headers=headers)
+print(response)
+print(response.json())
