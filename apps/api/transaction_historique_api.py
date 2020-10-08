36f1213846997b4fe08d997ab22e5a79ee915484
Williams de SOUZA <williamsko89@gmail.com>
2020-10-08 18:39:23 +0200

    test alter_list_data_to_serialize

--- apps/api/transaction_historique_api.py
+++ apps/api/transaction_historique_api.py
@@ -1,11 +1,12 @@
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from django.contrib.contenttypes.models import ContentType
 from tastypie.fields import ForeignKey
 from apps.api.agent_api import AgentResource
 from apps.transaction.models import Transaction
 from django.conf.urls import url
 from tastypie.utils import trailing_slash
 import json
-from django.http import HttpResponse
+from apps.kyc.models import Customer
 
 class TransactionHistoriqueResource(ModelResource):
     agent = ForeignKey(AgentResource, 'agent')
@@ -36,9 +37,12 @@ class TransactionHistoriqueResource(ModelResource):
         return bundle.obj.destination_country.currency.name
 
     def dehydrate(self, bundle):
-        bundle.data['custom_field'] = "Whatever you want"
-        return bundle
+        if bundle.obj.source_content_type == ContentType.objects.get_for_model(Customer)\
+                and bundle.obj.source_content_object:
+            bundle.data['source'] = '{0} {1}'.format(bundle.obj.source_content_object.informations.first_name,
+                                                     bundle.obj.source_content_object.informations.last_name)
 
+        return bundle
 
     def obj_get_list(self, bundle, **kwargs):
         return self.get_object_list(bundle.request)
