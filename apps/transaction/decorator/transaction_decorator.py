88f006f2db5ad3a0ba6118a6961f9bbd2433e733
Williams de SOUZA <williamsko89@gmail.com>
2020-11-24 18:10:39 +0100

    ajout recouvrement dans agent transactions

--- apps/transaction/decorator/transaction_decorator.py
+++ apps/transaction/decorator/transaction_decorator.py
@@ -3,6 +3,8 @@ from apps.core.utils.http import decode_jwt_token
 
 def agent_code_required(function):
     def modified_function(payload, token):
+        print (payload)
+        print (token)
         decoded_payload = decode_jwt_token(token, 'agent_api_secret_key')
         payload.update({'agent': {'code': decoded_payload.get('code')}})
         return function(payload, token)
